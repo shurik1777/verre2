65623726d50a0c59f64afbf3e2a6805f89376063
Wosmerik Alexander <shurik1777@gmail.com>
2024-06-28 10:23:15 +0300

    add admins category

--- cocktail/admin.py
+++ cocktail/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import CategoryCocktail, CategoryIngredient
+
+admin.site.site_header = 'Verre - сайт рецептов алкогольных напитков'
+admin.site.site_title = 'Администрирование сайта'
+admin.site.index_title = 'Администрирование сайта'
+
+
+@admin.register(CategoryCocktail)
+class CategoryCocktailAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', "slug")
+    list_display_links = ('name',)
+    list_editable = ["slug", ]
+    search_fields = ('name', "slug")
+    fields = [
+        "name",
+        "slug",
+    ]
+
+
+@admin.register(CategoryIngredient)
+class CategoryIngredientAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', "slug")
+    list_display_links = ('name',)
+    list_editable = ["slug", ]
+    search_fields = ('name', "slug")
+    fields = [
+        "name",
+        "slug",
+    ]
